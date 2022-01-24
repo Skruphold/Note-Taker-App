d88aa868a00ab2dbd2fa434d9bfecf0687bb7bb6
Skruphold <hunterco999@gmail.com>
2022-01-24 17:11:17 -0600

    added post method to apiRoute everything is working re entered the api in the get req too after deleting it.

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,12 +1,24 @@
 // const { response } = require('express');
+const { response } = require('express');
 const fs = require('fs');
 
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = function (app) {
-    app.get("/notes", (req, res) => {
+    app.get("/api/notes", (req, res) => {
         console.log("\nExecuting GET notes request");
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        res.json(data);
+    });
+
+    app.post("/api/notes", (req, res) => {
+        const addNote = req.body;
+        console.log("\nPOST request - new note:" + JSON.stringify(addNote));
+        addNote.id = uuidv4();
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        data.push(addNote);
+        fs.writeFileSync('./db/db.json', JSON.stringify(data));
+        console.log("successfully added note to the db.json file");
         res.json(data);
     })
 }
